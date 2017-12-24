79012cb4ad56dda68a18327e27ccb03697866330
Albert Gaikov <gaolbertas@gmail.com>
2017-12-24 22:27:53 +0300

    not important changes

--- src/main/java/by/haikou/bicycle_rental/controller/MainController.java
+++ src/main/java/by/haikou/bicycle_rental/controller/MainController.java
@@ -1,49 +1,49 @@
-//package by.haikou.bicycle_rental.controller;
-//
-//import by.haikou.bicycle_rental.command.CommandEnum;
-//import by.haikou.bicycle_rental.command.ICommand;
-//import by.haikou.bicycle_rental.command.factory.CommandFactory;
-//import by.haikou.bicycle_rental.exception.UnauthorizedException;
-//import by.haikou.bicycle_rental.util.MessageUtils;
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-//
-//public class MainController extends HttpServlet {
-//    private static final CommandFactory commandFactory = CommandFactory.getFactory();
-//    private final static Logger log = LogManager.getLogger(MainController.class);
-//
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        processRequest(req, resp);
-//    }
-//
-//    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        ICommand command = null;
-//        String commandName = req.getParameter("command");
-//        try {
-//            CommandEnum commandEnum = CommandEnum.getEnum(commandName);
-//            command = commandFactory.createCommand(commandEnum);
-//            command.execute(req, resp);
-//        } catch (UnauthorizedException exc) {
-//            log.error(exc);
-//            req.setAttribute("message", exc.getMessage());
-//            req.getRequestDispatcher("error_page.jsp").forward(req, resp);
-//        } catch (Exception exc) {
-//            log.error(exc);
-//            req.setAttribute("message", MessageUtils.getProperty("err.smth-error", req));
-//            req.getRequestDispatcher("error_page.jsp").forward(req, resp);
-//        }
-//    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        processRequest(req, resp);
-//    }
-//}
+package by.haikou.bicycle_rental.controller;
+
+import by.haikou.bicycle_rental.command.CommandEnum;
+import by.haikou.bicycle_rental.command.ICommand;
+import by.haikou.bicycle_rental.command.factory.CommandFactory;
+import by.haikou.bicycle_rental.exception.UnauthorizedException;
+import by.haikou.bicycle_rental.util.MessageUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class MainController extends HttpServlet {
+    private static final CommandFactory commandFactory = CommandFactory.getFactory();
+    private final static Logger log = LogManager.getLogger(MainController.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ICommand command = null;
+        String commandName = req.getParameter("command");
+        try {
+            CommandEnum commandEnum = CommandEnum.getEnum(commandName);
+            command = commandFactory.createCommand(commandEnum);
+            command.execute(req, resp);
+        } catch (UnauthorizedException exc) {
+            log.error(exc);
+            req.setAttribute("message", exc.getMessage());
+            req.getRequestDispatcher("error_page.jsp").forward(req, resp);
+        } catch (Exception exc) {
+            log.error(exc);
+            req.setAttribute("message", MessageUtils.getProperty("err.smth-error", req));
+            req.getRequestDispatcher("error_page.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,53 +1,26 @@
-create database bicycle_rent;
-use bicycle_rent;
-CREATE TABLE `Users` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `firstName` varchar(20) NOT NULL,
-  `lastName` varchar(20) NOT NULL,
-  `email` varchar(20) NOT NULL UNIQUE,
-  `password` varchar(20) NOT NULL);
-  
-INSERT INTO `Users` (`firstName`,`lastName`,`email`,`password`) VALUES ('Denis','Girich','Denis@tut.by','root');  
-INSERT INTO `Users` (`firstName`,`lastName`,`email`,`password`) VALUES ('Petr','Petrov','Petr@tut.by','root');  
-INSERT INTO `Users` (`firstName`,`lastName`,`email`,`password`) VALUES ('Ivan','Ivanov','Ivan@tut.by','root');  
-
-CREATE TABLE `Roles` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `roleName` VARCHAR(20) NOT NULL UNIQUE
+CREATE DATABASE bicycle_rent;
+USE bicycle_rent;
+CREATE TABLE `User` (
+  `id`        INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `firstName` VARCHAR(20) NOT NULL,
+  `lastName`  VARCHAR(20) NOT NULL,
+  `email`     VARCHAR(20) NOT NULL UNIQUE,
+  `password`  VARCHAR(20) NOT NULL
 );
 
-INSERT INTO `Roles` (`roleName`) VALUES ('admin');  
-INSERT INTO `Roles` (`roleName`) VALUES ('support'); 
-INSERT INTO `Roles` (`roleName`) VALUES ('client'); 
-
-CREATE TABLE `Users_to_Roles` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `fk_role_id` INTEGER NOT NULL,
-  `fk_user_id` INTEGER NOT NULL,
-
-  CONSTRAINT `fk_to_role` FOREIGN KEY (`fk_role_id`) REFERENCES `Roles` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-
-  CONSTRAINT `fk_to_user` FOREIGN KEY (`fk_user_id`) REFERENCES `Users` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    
-  CONSTRAINT `unique_role_user` UNIQUE (fk_role_id, fk_user_id)
-);
-
-INSERT INTO `Users_to_Roles` (`fk_user_id`, `fk_role_id`) VALUES ('1','1');
-INSERT INTO `Users_to_Roles` (`fk_user_id`, `fk_role_id`) VALUES ('2','2');
-INSERT INTO `Users_to_Roles` (`fk_user_id`, `fk_role_id`) VALUES ('2','3');
-INSERT INTO `Users_to_Roles` (`fk_user_id`, `fk_role_id`) VALUES ('3','3');
-
+INSERT INTO `User` (`firstName`, `lastName`, `email`, `password`, `role`)
+VALUES ('Albert', 'Haikou', 'gaolbertas@gmail.com', 'root', 'ADMINISTRATOR');
+INSERT INTO `User` (`firstName`, `lastName`, `email`, `password`, `role`)
+VALUES ('Petr', 'Petrov', 'Petr@tut.by', 'root', 'MANAGER');
+INSERT INTO `User` (`firstName`, `lastName`, `email`, `password`, `role`)
+VALUES ('Ivan', 'Ivanov', 'Ivan@tut.by', 'root', 'CLIENT');
 
 CREATE TABLE `Parking` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `street` varchar(20) NOT NULL UNIQUE
+  `id`     INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `street` VARCHAR(20) NOT NULL UNIQUE
 );
-  
-INSERT INTO `Parking` (`street`) VALUES ('Cобинова, 11');  
+
+INSERT INTO `Parking` (`street`) VALUES ('Cобинова, 11');
 INSERT INTO `Parking` (`street`) VALUES ('Волгоградская, 45');
 INSERT INTO `Parking` (`street`) VALUES ('Шишкина, 33');
 INSERT INTO `Parking` (`street`) VALUES ('Серова, 96');
@@ -55,55 +28,61 @@ INSERT INTO `Parking` (`street`) VALUES ('Шаранговича, 17');
 INSERT INTO `Parking` (`street`) VALUES ('Пушкина, 45');
 
 CREATE TABLE `Bikes` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `type` varchar(20) NOT NULL,
-  `model` varchar(20) NOT NULL,
-  `size` varchar(20) NOT NULL,
-   `isAvailable` BOOLEAN ,
-  `fk_parking_id` INTEGER NOT NULL,
-  
-   CONSTRAINT `fk_Bikes_to_Parking` FOREIGN KEY (`fk_parking_id`) REFERENCES `Parking` (`id`)
+  `id`            INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `type`          VARCHAR(20) NOT NULL,
+  `model`         VARCHAR(20) NOT NULL,
+  `size`          VARCHAR(20) NOT NULL,
+  `isAvailable`   BOOLEAN,
+  `fk_parking_id` INTEGER     NOT NULL,
+
+  CONSTRAINT `fk_Bikes_to_Parking` FOREIGN KEY (`fk_parking_id`) REFERENCES `Parking` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE
-  );
-INSERT INTO `Bikes` (`type`, `model`,`size`,`isAvailable`,`fk_Parking_id`) VALUES ('мужской', 'stels','20"','1','1');
-INSERT INTO `Bikes` (`type`, `model`,`size`,`isAvailable`,`fk_Parking_id`) VALUES ('женский', 'cube','16"','0','2');
-INSERT INTO `Bikes` (`type`, `model`,`size`,`isAvailable`,`fk_Parking_id`) VALUES ('мужской', 'smart','19"','1','3');
-INSERT INTO `Bikes` (`type`, `model`,`size`,`isAvailable`,`fk_Parking_id`) VALUES ('мужской', 'forward','18"','1','1');
-INSERT INTO `Bikes` (`type`, `model`,`size`,`isAvailable`,`fk_Parking_id`) VALUES ('женский', 'cube','19"','0','2');
-INSERT INTO `Bikes` (`type`, `model`,`size`,`isAvailable`,`fk_Parking_id`) VALUES ('мужской', 'smart','14"','1','4');
+);
+INSERT INTO `Bikes` (`type`, `model`, `size`, `isAvailable`, `fk_Parking_id`)
+VALUES ('мужской', 'stels', '20"', '1', '1');
+INSERT INTO `Bikes` (`type`, `model`, `size`, `isAvailable`, `fk_Parking_id`)
+VALUES ('женский', 'cube', '16"', '0', '2');
+INSERT INTO `Bikes` (`type`, `model`, `size`, `isAvailable`, `fk_Parking_id`)
+VALUES ('мужской', 'smart', '19"', '1', '3');
+INSERT INTO `Bikes` (`type`, `model`, `size`, `isAvailable`, `fk_Parking_id`)
+VALUES ('мужской', 'forward', '18"', '1', '1');
+INSERT INTO `Bikes` (`type`, `model`, `size`, `isAvailable`, `fk_Parking_id`)
+VALUES ('женский', 'cube', '19"', '0', '2');
+INSERT INTO `Bikes` (`type`, `model`, `size`, `isAvailable`, `fk_Parking_id`)
+VALUES ('мужской', 'smart', '14"', '1', '4');
 
 CREATE TABLE `SupportItem` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `fk_bikes_id` INTEGER NOT NULL,
+  `id`          INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `fk_bikes_id` INTEGER     NOT NULL,
   `description` VARCHAR(55) NOT NULL,
-  `status` BOOLEAN NOT NULL,
-  
+  `status`      BOOLEAN     NOT NULL,
+
   CONSTRAINT `fk_Support_to_Bikes` FOREIGN KEY (`fk_bikes_id`) REFERENCES `Bikes` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE
-  
+
 );
 
-INSERT INTO `SupportItem` (`fk_Bikes_id`,`description`,`status`) VALUES ('1','пробито колесо','1'); 
-INSERT INTO `SupportItem` (`fk_Bikes_id`,`description`,`status`) VALUES ('2','сломана педаль','0');
+INSERT INTO `SupportItem` (`fk_Bikes_id`, `description`, `status`) VALUES ('1', 'пробито колесо', '1');
+INSERT INTO `SupportItem` (`fk_Bikes_id`, `description`, `status`) VALUES ('2', 'сломана педаль', '0');
 
 
 CREATE TABLE `RentItem` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `fk_bikes_id` INTEGER NOT NULL,
-  `fk_users_id` INTEGER NOT NULL,
-  `date` TIMESTAMP NOT NULL,
-  `status` BOOLEAN NOT NULL,
-  
+  `id`          INTEGER   NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `fk_bikes_id` INTEGER   NOT NULL,
+  `fk_users_id` INTEGER   NOT NULL,
+  `date`        TIMESTAMP NOT NULL,
+  `status`      BOOLEAN   NOT NULL,
+
   CONSTRAINT `fk_RentItem_to_Bikes` FOREIGN KEY (`fk_bikes_id`) REFERENCES `Bikes` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-    
+
   CONSTRAINT `fk_RentItem_to_Users` FOREIGN KEY (`fk_users_id`) REFERENCES `Users` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE
-  
+
 );
   
 
